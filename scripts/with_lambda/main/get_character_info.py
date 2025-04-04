fd4a522a76fe1bf195824f9477e8857eec4d88cf
ProDays <joohjdays@gmail.com>
2025-04-04 12:03:38 +0900

    등록 직후 데이터 없을 때 발생하는 오류 수정

--- scripts/with_lambda/main/get_character_info.py
+++ scripts/with_lambda/main/get_character_info.py
@@ -76,36 +76,36 @@ def get_character_info(name, slot, period, today):
 
     period = len(data["date"])
 
-    # if period == 1:
-    #     current_level = data["level"][0]
+    if period == 1:  # 등록 직후 데이터가 없을 때
+        current_level = data["level"][0]
 
-    #     rank = None
-    #     if today == misc.get_today():
-    #         ranks = gri.get_current_rank_data()
-    #         for i, j in enumerate(ranks):
-    #             if (
-    #                 j["name"] == name
-    #                 and -0.1 < j["level"] - current_level < 0.1
-    #                 and misc.convert_job(j["job"]) == data["job"][0]
-    #             ):
-    #                 rank = i + 1
-    #                 break
-    #     else:
-    #         ranks = gri.get_rank_data(today)
-    #         for i, j in enumerate(ranks):
-    #             if (
-    #                 j["name"] == name
-    #                 and -0.1 < j["level"] - current_level < 0.1
-    #                 and j["job"] == data["job"][0]
-    #             ):
-    #                 rank = j["rank"]
-    #                 break
+        rank = None
+        if today == misc.get_today():
+            ranks = gri.get_current_rank_data()
+            for i, j in enumerate(ranks):
+                if (
+                    j["name"] == name
+                    and -0.1 < j["level"] - current_level < 0.1
+                    and misc.convert_job(j["job"]) == data["job"][0]
+                ):
+                    rank = i + 1
+                    break
+        else:
+            ranks = gri.get_rank_data(today)
+            for i, j in enumerate(ranks):
+                if (
+                    j["name"] == name
+                    and -0.1 < j["level"] - current_level < 0.1
+                    and j["job"] == data["job"][0]
+                ):
+                    rank = j["rank"]
+                    break
 
-    #     text_day = "지금" if today == misc.get_today() else today.strftime("%Y년 %m월 %d일")
-    #     text_rank = (
-    #         f"\n레벨 랭킹은 {rank}위에요." if rank is not None else "레벨 랭킹에는 아직 등록되지 않았어요."
-    #     )
-    #     return f"{text_day} {name}님의 레벨은 {current_level}이에요." + text_rank, None
+        text_day = "지금" if today == misc.get_today() else today.strftime("%Y년 %m월 %d일")
+        text_rank = (
+            f"\n레벨 랭킹은 {rank}위에요." if rank is not None else "레벨 랭킹에는 아직 등록되지 않았어요."
+        )
+        return f"{text_day} {name}님의 레벨은 {current_level}이에요." + text_rank, None
 
     all_character_avg = get_all_character_avg(period, today)
 
@@ -387,7 +387,7 @@ def get_charater_rank_history(name, period, today):
     if period == 0:
         return f"{name}님의 랭킹 정보가 없어요.", None
 
-    elif period == 1:
+    elif period == 1:  # 등록 직후 데이터가 없을 때
         text_day = "지금" if today == misc.get_today().strftime("%Y-%m-%d") else today_text
         text_rank = f"{name}님의 랭킹은 {101 - data[0]['rank']}위에요."
         return f"{text_day} {text_rank}", None
@@ -619,8 +619,8 @@ if __name__ == "__main__":
     today = misc.get_today()
 
     # print(get_charater_rank_history("prodays", 5, today))
-    print(get_character_info("prodays", None, 7, today))
+    print(get_character_info("steve", None, 7, today))
     # print(get_current_character_data("ProDays"))
-    # print(get_character_data("ProDays", 1, 5, today))
+    # print(get_character_data("steve", 1, 7, today))
 
     pass
